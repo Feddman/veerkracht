e96faad7ee2718bf1aa07da6bee99bf953de30d9
SilvinvanHaestregt <D289981@edu.curio.nl>
2024-02-29 14:55:16 +0100

    Update 2024_02_29_131648_create_questions_table.php

--- database/migrations/2024_02_29_131648_create_questions_table.php
+++ database/migrations/2024_02_29_131648_create_questions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->enum('category', ['Mijn lichaam', 'Mijn gevoelens en gedachten', 'Nu en later', 'Lekker in je vel zitten', 'Meedoen', 'Dagelijks leven']);
             $table->timestamps();
         });
     }
