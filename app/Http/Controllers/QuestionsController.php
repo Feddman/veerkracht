9c9c7dae32064e101441e2aad50df22ab59d2a5a
SilvinvanHaestregt <D289981@edu.curio.nl>
2024-03-07 14:44:01 +0100

    Profielcheck

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
+use App\Models\Answer;
 use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
@@ -9,5 +11,14 @@ class QuestionsController extends Controller
     public function store(Request $request)
     {
         var_dump($request->all());
+        // make the answers by looping through the request data and creating a new Answer for each question and also get the question to get the ID
+        foreach($request->all() as $key => $value) {
+            $question = Question::where('id', $value)->first();
+            // Answer::create([
+            //     'question_id' => $question->id,
+            //     'answer' => $value,
+            //     'user_id' => auth()->user()->id
+            // ]);
+        }
     }
 }
