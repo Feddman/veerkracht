924242b7a422e66d5818d9d75dbc0ab354bb45dd
Pluk Zwaal <Plukky.zwaal@gmail.com>
2024-03-15 12:22:20 +0100

    PLATTGEROND UPDATE

--- resources/views/plattegrond.blade.php
+++ resources/views/plattegrond.blade.php
@@ -15,7 +15,7 @@
                     reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div class="body-blok1-content-links"  style="text-align: center">
-                <img class="iph" src="{{ asset('storage/img/plattegrond.png') }}" alt="">
+                <img class="plattegrond" src="{{ asset('storage/img/plattegrond.png') }}" alt="">
             </div>
         </div>
     </div>
